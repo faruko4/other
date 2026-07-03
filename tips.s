89b53b25da00da6048fe42a3a65d130e567f0964
Faruko <kiaffddd@gmail.com>
2026-07-03 17:19:59 +0000

    Create tips.s

---
+++ tips.s
@@ -0,0 +1,3 @@
+Deepseek just gave me some  tips 
+1 -> When you read a spec or something else, make sure that u are pointing on every single line with your finger or a pen ,even just 
+a mouse pointer...this increase the reading speed 2x
